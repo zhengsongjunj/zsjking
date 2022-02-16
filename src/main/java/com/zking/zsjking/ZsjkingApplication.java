5f0dab0efa5dbea6a26dfeeb937c208bf80a7eba
zhengsongjun <zheng_songjun@venusgroup.com.cn>
2022-02-16 11:23:31 +0800

    feat: 初始化sp项目

---
+++ src/main/java/com/zking/zsjking/ZsjkingApplication.java
@@ -0,0 +1,13 @@
+package com.zking.zsjking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsjkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZsjkingApplication.class, args);
+    }
+
+}
