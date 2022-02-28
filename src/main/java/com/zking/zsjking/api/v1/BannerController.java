4452b04a63bdf95e9ddcd20ca59d3614853f7717
zhengsongjun <zheng_songjun@venusgroup.com.cn>
2022-02-28 15:08:03 +0800

    feat: 尝试取消默认的实例化

--- src/main/java/com/zking/zsjking/api/v1/BannerController.java
+++ src/main/java/com/zking/zsjking/api/v1/BannerController.java
@@ -1,6 +1,7 @@
 package com.zking.zsjking.api.v1;
 import com.zking.zsjking.sample.hero.Diana;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import java.io.DataInput;
 import java.io.IOException;
 
 @RestController
+@Lazy
 @RequestMapping("/v1/banner")
 public class BannerController {
     @Autowired
--- src/main/java/com/zking/zsjking/sample/hero/Diana.java
+++ src/main/java/com/zking/zsjking/sample/hero/Diana.java
@@ -1,9 +1,15 @@
 package com.zking.zsjking.sample.hero;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class Diana {
+    public Diana () {
+        System.out.println("我被实例化了！");
+    }
+
     public void q() {
         System.out.println("dian q");
     }
